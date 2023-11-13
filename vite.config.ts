800802d8e2d6d886dd82af07c7373e0f66f32518
도경 <lee7198@naver.com>
2023-11-14 02:07:37 +0900

    edit url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/lee7198/IndustryClient.git/', // ex: /my_project/
+  base: '/IndustryClient/', // ex: /my_project/
   plugins: [react()],
 });
