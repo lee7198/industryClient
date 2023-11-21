69fa22fdc928adf7611fdb976afb86380c2f3f49
도경 <lee7198@naver.com>
2023-11-21 17:54:18 +0900

    delete dark mode styles

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,7 @@ export default function Loading({ className }: { className?: string }) {
     <div className={className} role="status">
       <svg
         aria-hidden="true"
-        className="h-8 w-8 animate-spin fill-blue-600 text-gray-200/30 dark:text-gray-600"
+        className="h-8 w-8 animate-spin fill-blue-600 text-gray-200/30"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
